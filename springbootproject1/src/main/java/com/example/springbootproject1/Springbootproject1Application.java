85b3b7c5e98cd50ff917e7e6570f86eaf027eed7
Priyankaishwarkatti <155129187+Priyankaishwarkatti@users.noreply.github.com>
2025-03-01 16:06:21 +0530

    Add files via upload

---
+++ springbootproject1/src/main/java/com/example/springbootproject1/Springbootproject1Application.java
@@ -0,0 +1,15 @@
+package com.example.springbootproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootproject1Application.class, args);
+		System.out.println("Hello World!!!!");
+		
+	}
+
+}
